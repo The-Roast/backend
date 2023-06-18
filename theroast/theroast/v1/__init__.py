56287a3b7344758019de9c4d5d093ee62eabfae6
michael-beaverhealth <133283761+michael-beaverhealth@users.noreply.github.com>
2023-06-18 10:21:22 -0700

    added improved prompts

--- theroast/theroast/v1/__init__.py
+++ theroast/theroast/v1/__init__.py
@@ -5,7 +5,7 @@ from theroast.config import OPENAI_API_KEY
 from theroast.theroast.v1.reqs import extract_request, cluster_request, section_request, collate_request
 import json
 
-PERSONALITY = "snarky and sarcastic"
+PERSONALITY = "smart and sarcastic but not condescending or overbearing, and never immature"
 
 ns = NewsScraper()
 articles = ns.get_everything(q = "politics")
