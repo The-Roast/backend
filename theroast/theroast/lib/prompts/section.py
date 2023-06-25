7505d5860ab8efef12b01640b40b16f25ba47e46
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-06-25 13:36:40 -0700

    Update section.py

--- theroast/theroast/lib/prompts/section.py
+++ theroast/theroast/lib/prompts/section.py
@@ -10,7 +10,6 @@ SECTION_PROMPT = dedent('''\
     There should be no introduction, conclusion, hooks and signposting in the section. \
     The personality you have should be subtly evident in the tone and mood of the writing style in the section. \
     If you use information you must cite the article you extracted the information from using [${number}] notation where the number is the index number of the article in the list provided. \
-    If you have multiple citations for one sentence cite it as [${number}] for each individual article (e.g. [1][2][3]).
 
     I also want you to create a section header for the section.
 
