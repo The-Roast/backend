5718071c719511b9435cca7b16cb80dceefd0362
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-07-01 07:06:41 -0700

    added prompts

--- theroast/theroast/lib/prompts/section.py
+++ theroast/theroast/lib/prompts/section.py
@@ -3,21 +3,30 @@ from textwrap import dedent
 from . import Prompt, SYSTEM_PROMPT
 
 SECTION_PROMPT = dedent('''\
-    Given the list of articles separated by <>, I want you to create a section of a newsletter solely based on the information within the list of articles provided.
+    Given a list of article links separated by <>, assemble an engaging and insightful section for a newsletter. \
+    This section should distill the essential points from each article in the list, resulting in a comprehensive summary that makes the original articles redundant for understanding.
 
-    This section should be detailed and comprehensive detailing the main points of each article in list of articles that a reader of a newsletter would find interesting to learn about. \
-    The section should not require the reader to view the article to understand anything brought up in the section. \
-    There should be no introduction, conclusion, hooks and signposting in the section. \
-    The personality you have should be subtly evident in the tone and mood of the writing style in the section. \
-    If you use information you must cite the article you extracted the information from using [${number}] notation where the number is the index number of the article in the list provided. \
+    Eliminate elements such as introduction, conclusion, hooks, and signposting, but allow your personality to subtly influence the tone and mood of the writing in alignment with the dynamic system prompt.
 
-    I also want you to create a section header for the section.
+    Cite any extracted information from the articles using the notation [${number}], where the number corresponds to the article's index in the list. \
+    Aim for a seamless integration of these citations within the text.
 
-    I want your response to be formatted as strictly a JSON with the following structure:
+    Additionally, formulate an engaging title for this section. The response must adhere to the JSON format that can be parsed by the Python json library, represented as:
     {
-        "title": "Section header you generated",
-        "body": "Final section you generated"
-    }''')
+    "title": "Generated section title",
+    "body": "Crafted section content"
+    }
+
+    The tone, ordering of the information, and the enticing nature of the section title should reflect the subject matter of the articles. \
+    Incorporate both summarizations and direct quotes from the articles, favoring direct quotes where applicable.
+
+    Ensure the newsletter content equally focuses on individual article content and also draws connections and synthesizes information across the articles. \
+    Specific aspects such as the author's opinions, study results, or interview highlights should be emphasized where relevant. \
+    Any biased aspects should be presented as the articles' opinions. \
+    In cases of conflicting viewpoints across articles, present both perspectives, clearly indicating them as opinions from the respective articles.
+
+    There is no hard limit on the length of the section, but maintain a balance to ensure it remains engaging to the reader.\
+    ''')
 REFORMAT_SECTION_PROMPT = dedent('''\
     Given the broken JSON separated by $$, reformat it so that it follows the formatting and is a parseable JSON. Format your response as a JSON with the following structure:
     {
