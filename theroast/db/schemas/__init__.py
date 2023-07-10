29e131541674af9ca5f9261a0b9408ce9ba20826
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-07-09 17:43:35 -0700

    db upgrades

--- theroast/db/schemas/__init__.py
+++ theroast/db/schemas/__init__.py
@@ -28,23 +28,29 @@ def create_settings():
         "personality": "normal"
     }
 
-class Users(db.Model):
+newsletter_article = db.Table(
+    "newsletter_article",
+    ss.Column("newsletter", ss.ForeignKey("newsletters.uuid"), primary_key = True),
+    ss.Column("article", ss.ForeignKey("articles.uuid"), primary_key = True)
+)
+
+class Users(db.Model): # checked
 
     __tablename__ = "users"
     __table_args__ = {"extend_existing": True}
 
-    id = ss.Column("uuid", st.UUID(as_uuid = True), primary_key = True, index = True, default = uuid.uuid4)
+    uuid = ss.Column("uuid", st.UUID(as_uuid = True), primary_key = True, index = True, default = uuid.uuid4)
 
-    digests = db.relationship("Digests")
+    digests = db.relationship("Digests", back_populates = "user", cascade = "all, delete, delete-orphan")
 
-    first_name = ss.Column("first_name", st.String, unique = False, nullable = False)
-    last_name = ss.Column("last_name", st.String, unique = False, nullable = False)
+    first_name = ss.Column("first_name", st.String, nullable = False)
+    last_name = ss.Column("last_name", st.String, nullable = False)
     email = ss.Column("email", st.String, unique = True, index = True, nullable = False)
-    password = ss.Column("password", st.String, unique = False, nullable = False)
+    password = ss.Column("password", st.String, nullable = False)
 
-    created_at = ss.Column("created_at", st.DateTime, unique = False, nullable = False, default = datetime.datetime.utcnow)
-    updated_at = ss.Column("updated_at", st.DateTime, unique = False, nullable = False, default = datetime.datetime.utcnow, onupdate = datetime.datetime.utcnow)
-    deleted_at = ss.Column("deleted_at", st.DateTime, unique = False, nullable = True)
+    created_at = ss.Column("created_at", st.DateTime, nullable = False, default = datetime.datetime.utcnow)
+    updated_at = ss.Column("updated_at", st.DateTime, nullable = False, default = datetime.datetime.utcnow, onupdate = datetime.datetime.utcnow)
+    deleted_at = ss.Column("deleted_at", st.DateTime)
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf-8')
@@ -54,50 +60,100 @@ class Users(db.Model):
 
     def as_dict(self):
         return {
-            "firstName": self.first_name,
-            "lastName": self.last_name,
-            "email": self.email,
+            "uuid": self.uuid,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email
         }
 
-class Digests(db.Model):
+class Digests(db.Model): # checked
 
     __tablename__ = "digests"
     __table_args__ = {"extend_existing": True}
 
     uuid = ss.Column("uuid", st.UUID(as_uuid = True), primary_key = True, index = True, default = uuid.uuid4)
 
-    name = ss.Column("name", st.String, unique = False, nullable = True)
+    user = db.relationship("Users", back_populates = "digests")
+    user_uuid = ss.Column("user_uuid", st.UUID(as_uuid = True), ss.ForeignKey("users.uuid"))
+    
+    newsletters = db.relationship("Newsletters", back_populates = "digest")
+
+    name = ss.Column("name", st.String)
     settings = ss.Column(
         "settings",
-        NestedMutableJson, unique = False, nullable = False,
+        NestedMutableJson, nullable = False,
         default = create_settings
     )
-    color = ss.Column("color", st.String(7), unique = False, nullable = False, default = create_color)
+    color = ss.Column("color", st.String(7), nullable = False, default = create_color)
 
-    created_at = ss.Column("created_at", st.DateTime, unique = False, nullable = False, default = datetime.datetime.utcnow)
-    updated_at = ss.Column("updated_at", st.DateTime, unique = False, nullable = False, default = datetime.datetime.utcnow, onupdate = datetime.datetime.utcnow)
-    deleted_at = ss.Column("deleted_at", st.DateTime, unique = False, nullable = True)
+    created_at = ss.Column("created_at", st.DateTime, nullable = False, default = datetime.datetime.utcnow)
+    updated_at = ss.Column("updated_at", st.DateTime, nullable = False, default = datetime.datetime.utcnow, onupdate = datetime.datetime.utcnow)
+    deleted_at = ss.Column("deleted_at", st.DateTime)
 
     def as_dict(self):
         return {
             "uuid": self.uuid,
+            "user": self.user_uuid,
             "name": self.name,
-            "contentSources": ", ".join(dict(self.settings)["sources"]),
-            "interests": ", ".join(dict(self.settings)["interests"]),
+            "sources": dict(self.settings)["sources"],
+            "interests": dict(self.settings)["interests"],
             "personality": dict(self.settings)["personality"],
-            "color": {
-                "hex": self.color
-            }
+            "color": self.color
         }
 
-class Newsletter(db.Model):
+class Newsletters(db.Model):
 
     __tablename__ = "newsletters"
     __table_args__ = {"extend_existing": True}
 
     uuid = ss.Column("uuid", st.UUID(as_uuid = True), primary_key = True, index = True, default = uuid.uuid4)
 
-    data = ss.Column("data", NestedMutableJson, unique = False, nullable = False)
-    articles = db.relationship("Articles")
+    articles = db.relationship("Articles", secondary = newsletter_article, back_populates = "newsletters")
+    
+    digest = db.relationship("Digests", back_populates = "newsletters")
+    digest_uuid = ss.Column("digest_uuid", st.UUID(as_uuid = True), ss.ForeignKey("digests.uuid", ondelete = "SET NULL"), nullable = True)
 
-    created_at = ss.Column("created_at", st.DateTime, unique = False, nullable = False, default = datetime.datetime.utcnow)
+    data = ss.Column("data", NestedMutableJson, nullable = False)
+    html = ss.Column("html", st.String, nullable = False)
+
+    created_at = ss.Column("created_at", st.DateTime, nullable = False, default = datetime.datetime.utcnow)
+
+    def as_dict(self, exclude_data = True):
+        d = {
+            "uuid": self.uuid,
+            "digest_uuid": self.digest_uuid,
+            "html": self.html,
+            "created_at": self.created_at
+        }
+        if not exclude_data:
+            d["data"] = dict(self.data)
+        return d
+
+class Articles(db.Model):
+
+    __tablename__ = "articles"
+    __table_args__ = {"extend_existing": True}
+
+    uuid = ss.Column("uuid", st.UUID(as_uuid = True), primary_key = True, index = True, default = uuid.uuid4)
+
+    newsletters = db.relationship("Newsletters", secondary = newsletter_article, back_populates = "articles")
+
+    source = ss.Column("source", st.String, index = True)
+    author = ss.Column("author", st.String, index = True)
+    title = ss.Column("title", st.String)
+    content = ss.Column("content", st.String, nullable = False)
+    url = ss.Column("url", st.String, index = True, nullable = False)
+
+    published_at = ss.Column("published_at", st.DateTime, nullable = False)
+    created_at = ss.Column("created_at", st.DateTime, nullable = False, default = datetime.datetime.utcnow)
+
+    def as_dict(self):
+        return {
+            "uuid": self.uuid,
+            "source": self.source,
+            "author": self.author,
+            "title": self.title,
+            "content": self.content,
+            "url": self.url,
+            "published_at": self.published_at
+        }
