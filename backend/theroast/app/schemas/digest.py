d55bf17dc5ab3125b1921a8cda7bc1f661d977fa
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-07-12 18:34:59 -0700

    Serialization added for User and Digest

--- backend/theroast/app/schemas/digest.py
+++ backend/theroast/app/schemas/digest.py
@@ -1,2 +1,31 @@
-from typing import Optional
-from pydantic import
+from typing import Optional, Dict
+from pydantic import BaseModel, UUID4, EmailStr, ConfigDict, Json
+
+class SettingsModel(BaseModel):
+    interests: list
+    sources: list
+    personality: str
+
+class DigestBase(BaseModel):
+    name: Optional[str] = None
+    settings: Optional[Dict[str, SettingsModel]] = None
+    color: Optional[str] = None
+    is_enabled: Optional[bool] = True
+
+class DigestCreate(DigestBase):
+    user: UUID4
+    name: str
+    settings: Dict[str, SettingsModel]
+    color: str
+
+class DigestUpdate(DigestBase):
+    uuid: UUID4
+    user: UUID4
+
+class DigestInDBBase(DigestBase):
+    model_config = ConfigDict(from_attributes=True)
+    uuid: UUID4
+    user: UUID4
+
+class Digest(DigestInDBBase):
+    name: str
--- backend/theroast/app/schemas/user.py
+++ backend/theroast/app/schemas/user.py
@@ -2,25 +2,24 @@ from typing import Optional
 from pydantic import BaseModel, UUID4, EmailStr, ConfigDict
 
 class UserBase(BaseModel):
-    first_name: str
-    last_name: str
-    email: Optional[EmailStr]
+    first_name: Optional[str]
+    last_name: Optional[str]
+    email: Optional[EmailStr] = None
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
 
 class UserCreate(UserBase):
+    first_name: str
+    last_name: str
     email: EmailStr
     password: str
 
 class UserUpdate(UserBase):
     uuid: UUID4
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
-    email: Optional[str] = None
     password: Optional[str] = None
 
 class UserInDBBase(UserBase):
-    model_config = ConfigDict(from_attributes = True)
+    model_config = ConfigDict(from_attributes=True)
     uuid: UUID4
 
 class User(UserInDBBase):
