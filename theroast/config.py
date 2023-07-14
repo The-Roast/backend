438d7144f576a19a77c3283dfac4c5071db08664
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-07-14 00:20:21 -0700

    Added working email implementation

--- theroast/config.py
+++ theroast/config.py
@@ -10,8 +10,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 
-ENV = os.getenv("FLASK_ENV")
-DEBUG = ENV == "development"
+DEBUG = os.getenv("FLASK_ENV") == "development"
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
@@ -31,5 +30,5 @@ MAIL_SERVER = os.getenv("MAIL_SERVER")
 MAIL_PORT = os.getenv("MAIL_PORT")
 MAIL_USERNAME = os.getenv("MAIL_USERNAME")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
-MAIL_USE_SSL = os.getenv("MAIL_USE_SSL")
+MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == 'True'
+MAIL_USE_SSL = os.getenv("MAIL_USE_SSL") == 'True'
