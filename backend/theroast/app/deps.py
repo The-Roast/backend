ca5a9dec024274f055190a0e4218cc2c45d719e7
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-07-17 22:04:45 -0700

    Added dependencies needed for JWT authentication

--- backend/theroast/app/deps.py
+++ backend/theroast/app/deps.py
@@ -13,7 +13,7 @@ from theroast.config import server_config
 from theroast.db.session import SessionLocal
 
 reusable_oauth2 = OAuth2PasswordBearer(
-    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+    tokenUrl=f"{server_config.API_V1_STR}/login/access-token"
 )
 
 
@@ -27,10 +27,10 @@ def get_db() -> Generator:
 
 def get_current_user(
     db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
-) -> models.User:
+) -> tables.User:
     try:
         payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+            token, server_config.SECRET_KEY, algorithms=[security.ALGORITHM]
         )
         token_data = schemas.TokenPayload(**payload)
     except (jwt.JWTError, ValidationError):
@@ -43,18 +43,16 @@ def get_current_user(
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-
 def get_current_active_user(
-    current_user: models.User = Depends(get_current_user),
-) -> models.User:
+    current_user: tables.User = Depends(get_current_user),
+) -> tables.User:
     if not crud.user.is_active(current_user):
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
-
 def get_current_active_superuser(
-    current_user: models.User = Depends(get_current_user),
-) -> models.User:
+    current_user: tables.User = Depends(get_current_user),
+) -> tables.User:
     if not crud.user.is_superuser(current_user):
         raise HTTPException(
             status_code=400, detail="The user doesn't have enough privileges"
