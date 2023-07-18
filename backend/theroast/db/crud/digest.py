117a8cc4a86633be409240d35e960c3ca990537b
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-07-17 20:43:15 -0700

    Added crud digest

---
+++ backend/theroast/db/crud/digest.py
@@ -0,0 +1,46 @@
+from typing import Any, Dict, Optional, Union
+from uuid import UUID
+from sqlalchemy.orm import Session
+from sqlalchemy import select, insert
+
+from theroast.db.crud.base import CRUDBase
+from theroast.db.tables.digest import Digest, create_color
+from theroast.app.schemas.digest import DigestCreate, DigestUpdate
+
+
+class CRUDDigest(CRUDBase[Digest, DigestCreate, DigestUpdate]):
+
+    def get_by_email(self, db: Session, *, email: str) -> Optional[Digest]:
+        stmt = select(Digest).where(Digest.email == email)
+        return db.execute(stmt).first()
+
+    def create_with_owner(self, db: Session, *, obj_in: DigestCreate, user_uuid: UUID) -> Digest:
+        stmt = insert(Digest).values(
+            user_uuid=obj_in.user_uuid if obj_in.user_uuid else user_uuid,
+            name=obj_in.name,
+            interests=obj_in.interests,
+            sources=obj_in.sources,
+            personality=obj_in.personality,
+            color=create_color(obj_in.color),
+            is_enabled=obj_in.is_enabled
+        )
+        db_obj = db.execute(stmt).first()
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def get_multi_by_owner(self, db: Session, *, user_uuid, skip: Optional[int], limit: Optional[int]):
+        stmt = select(Digest).where(Digest.user_uuid == user_uuid)
+        if skip and limit:
+            stmt = stmt.offset(skip).limit(limit)
+        elif skip:
+            stmt = stmt.offset(skip)
+        elif limit:
+            stmt = stmt.limit(limit)
+        return db.execute(stmt).fetchall()
+
+    def is_enabled(self, Digest: Digest) -> bool:
+        return Digest.is_enabled
+
+
+digest = CRUDDigest(Digest)
