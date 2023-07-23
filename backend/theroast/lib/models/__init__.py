be955ebdeac9678ced6137db7564e3771437f37d
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-07-22 23:49:33 -0700

    Updated pipeline for extracting and clustering

--- backend/theroast/lib/models/__init__.py
+++ backend/theroast/lib/models/__init__.py
@@ -3,7 +3,7 @@ from langchain.chat_models import ChatOpenAI, ChatAnthropic
 from sentence_transformers import SentenceTransformer
 from theroast.config import api_config
 
-COHERE = cohere.Client(COHERE_API_KEY)
+COHERE = cohere.Client(api_config.COHERE_API_KEY)
 ST_ENCODER = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-CLAUDE = ChatAnthropic(anthropic_api_key = ANTHROPIC_API_KEY, model = "claude-1", temperature = 0.4)
-GPT = ChatOpenAI(openai_api_key = OPENAI_API_KEY, model = "gpt-3.5-turbo-16k", temperature = 0.5)
+CLAUDE = ChatAnthropic(anthropic_api_key=api_config.ANTHROPIC_API_KEY, model="claude-2", temperature=0.4)
+GPT = ChatOpenAI(openai_api_key=api_config.OPENAI_API_KEY, model="gpt-3.5-turbo-16k", temperature=0.5)
--- backend/theroast/lib/pipeline/batch.py
+++ backend/theroast/lib/pipeline/batch.py
@@ -1,99 +1,98 @@
-from collections import defaultdict
 import math
-import umap
+from collections import defaultdict
+
 import hdbscan
+import umap
 from sklearn.metrics.pairwise import cosine_similarity
-from ..models import co, st
 
-def cluster(embeddings, n_neighbors = 5, n_components = 50, min_cluster_size = 2, random_state = None):
+from ..models import COHERE, ST_ENCODER
 
+
+def cluster(embeddings, n_neighbors=5, n_components=50, min_cluster_size=2, random_state=None):
+    """Cluster the embeddings using UMAP and HDBSCAN."""
     _embeddings = umap.UMAP(
         n_neighbors=n_neighbors,
         n_components=n_components, 
         metric='cosine', 
-        random_state=random_state).fit_transform(embeddings)
+        random_state=random_state
+    ).fit_transform(embeddings)
     clusters = hdbscan.HDBSCAN(
-        min_cluster_size = min_cluster_size,
+        min_cluster_size=min_cluster_size,
         metric='euclidean', 
-        cluster_selection_method='eom').fit(_embeddings)
-
+        cluster_selection_method='eom'
+    ).fit(_embeddings)
     return clusters
 
 
 def parse_clusters(labels, articles):
-
+    """Group articles into clusters based on the labels."""
     clusters = defaultdict(list)
-    for i, k in enumerate(labels):
-        clusters[k].append(articles[i])
+    for i, label in enumerate(labels):
+        clusters[label].append(articles[i])
     
     return clusters
 
-def parse_rankings(rankings):
 
-    output = {}
-    for i, r in enumerate(rankings):
-        output[r.document['text']] = r.relevance_score
-    
+def parse_rankings(rankings):
+    """Parse rankings into a dictionary with text as keys and relevance_score as values."""
+    output = {ranking.document['text']: ranking.relevance_score for ranking in rankings}
     return output
+
 
 def rerank(articles, q):
-
-    rankings = co.rerank(query = q, model = "rerank-english-v2.0", documents = articles)
-
+    """Rerank the articles using the specified model."""
+    rankings = COHERE.rerank(query=q, model="rerank-english-v2.0", documents=articles)
     return rankings
 
+
 def rank_clusters(clusters, rankings):
-    
+    """Rank clusters based on the average relevance score of its articles."""
     output = []
-    for k, v in clusters.items():
-        vbr = [(a, rankings[a]) for a in v]
-        vbr.sort(key = lambda x: x[1], reverse = True)
-        total = sum([n for _, n in vbr]) / len(vbr)
-        output.append(([a for a, _ in vbr], total))
-    output.sort(key = lambda x: x[1], reverse = True)
-
+    for cluster, articles in clusters.items():
+        vbr = [(article, rankings[article]) for article in articles]
+        vbr.sort(key=lambda x: x[1], reverse=True)
+        average_score = sum(score for _, score in vbr) / len(vbr)
+        output.append(([article for article, _ in vbr], average_score))
+    output.sort(key=lambda x: x[1], reverse=True)
     return output
 
-def filter_by_rank(clusters, threshold = 0):
 
-    return [c for c in clusters if c[1] > threshold]
+def filter_by_rank(clusters, threshold=0):
+    return [cluster for cluster in clusters if cluster[1] > threshold]
 
-def extract(clusters, articles, proportion, floor = 0.75):
 
+def extract(cluster_articles, article_embeddings, proportion, floor=0.75):
     output = {}
-    for k, v in clusters.items():
-        _extract = []
-        for a in v:
-            unique = True
-            for ea in _extract:
-                if cosine_similarity([articles[a]], [articles[ea]]) > floor:
-                    unique = False
-                    break
-            if unique: _extract.append(a)
-        if len(_extract) > len(v) * proportion:
-            _extract = _extract[0: math.ceil(len(v) * proportion)]
-        output[k] = _extract
-    
+    for cluster, articles in cluster_articles.items():
+        _extracted_articles = []
+        for article in articles:
+            if all(cosine_similarity([article_embeddings[article]], 
+                                      [article_embeddings[already_extracted]])[0][0] <= floor 
+                   for already_extracted in _extracted_articles):
+                _extracted_articles.append(article)
+        if len(_extracted_articles) > len(articles) * proportion:
+            _extracted_articles = _extracted_articles[:math.ceil(len(articles) * proportion)]
+        output[cluster] = _extracted_articles
     return output
 
-def extract_and_cluster(articles, q, target = 20):
 
-    embeddings = st.encode(articles)    
-    article__embedding = {a: embeddings[i] for i, a in enumerate(articles)}
-    
-    clusters = cluster(embeddings, n_neighbors = 15, n_components = 50, min_cluster_size = 3, random_state = 42)
-    cluster__articles = parse_clusters(clusters.labels_, articles)    
-    if -1 in cluster__articles.keys():
-        del cluster__articles[-1]
-
+def extract_and_cluster(articles, q, target=20):
+    embeddings = ST_ENCODER.encode(articles)    
+    article_embedding = {article: embeddings[i] for i, article in enumerate(articles)}
+    clusters = cluster(
+        embeddings, 
+        n_neighbors=15, 
+        n_components=5,
+        min_cluster_size=3, 
+        random_state=42
+    )
+    cluster_articles = parse_clusters(clusters.labels_, articles)
+    if -1 in cluster_articles.keys():
+        del cluster_articles[-1]
     rankings = rerank(articles, q)
-    article__ranking = parse_rankings(rankings)
-
-    cbr = rank_clusters(cluster__articles, article__ranking)
+    article_ranking = parse_rankings(rankings)
+    cbr = rank_clusters(cluster_articles, article_ranking)
     cbr = filter_by_rank(cbr)
-
-    EXTRACTION_PROPORTION = target / sum([len(c[0]) for c in cbr])
-
-    output = extract(cluster__articles, article__embedding, proportion = EXTRACTION_PROPORTION)
-
+    extraction_proportion = target / sum(len(c[0]) for c in cbr)
+    output = extract(cluster_articles, article_embedding, proportion=extraction_proportion)
     return output
