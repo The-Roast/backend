84a670a18cb3ce6ed6088604bc0166438d0a0257
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-07-25 00:24:29 -0700

    Basic (very shitty) logic for update_newsletter added

--- backend/theroast/app/api/v1/endpoints/newsletter.py
+++ backend/theroast/app/api/v1/endpoints/newsletter.py
@@ -98,18 +98,21 @@ def update_newsletter(
     newsletter_in: schemas.NewsletterUpdate,
     current_user: base.User = Depends(deps.get_current_active_user)
 ) -> Any:
-    newsletter = crud.newsletter.get(db, uuid=uuid)
+    newsletter = crud.newsletter.get(db, uuid=newsletter_in.uuid)
+    digest: base.Digest = newsletter.digest
     if not newsletter:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
             detail="Newsletter not found."
         )
-    if not crud.user.is_superuser(current_user) and newsletter.user_uuid != current_user.uuid:
+    if not crud.user.is_superuser(current_user) and digest.user_uuid != current_user.uuid:
         raise HTTPException(
             status_code=HTTPStatus.FORBIDDEN,
             detail="User does not have enough priviledges and does not own newsletter."
         )
-    newsletter = crud.newsletter.update(db, db_obj=newsletter, obj_in=newsletter_in)
+    sections, structure = pipeline.generate_newsletter(digest)
+    newsletter_data = pipeline.restructure(sections, structure)
+    newsletter = crud.newsletter.create_with_data(db, obj_in=newsletter_in, data=newsletter_data)
     return newsletter
 
 @router.delete("/{uuid}", response_model=schemas.Newsletter)
