3aa3d1e40f420a1d729fe3efde9ada0ef8b05f59
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-07-25 00:48:35 -0700

    Added all needed files and endpoints (UNTESTED)

--- backend/theroast/app/main.py
+++ backend/theroast/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from theroast.app.api.v1.views import api_router
+from theroast.app.auth import router as auth_router
 from theroast.config import server_config
 
 app = FastAPI(
@@ -17,4 +19,5 @@ if server_config.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
-app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(api_router, prefix=server_config.API_V1_STR)
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
