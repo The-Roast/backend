e2e5aa139286f46a9f2474c8d08fb19585a873d8
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-08-02 23:02:55 -0700

    Update user.py

--- theroast/db/crud/user.py
+++ theroast/db/crud/user.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, Optional, Union
 
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, insert
 
 from theroast.core.security import get_password_hash, verify_password
@@ -10,11 +10,11 @@ from theroast.app.schemas.user import UserCreate, UserUpdate
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
 
-    def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
+    async def get_by_email(self, db: AsyncSession, *, email: str) -> Optional[User]:
         stmt = select(User).where(User.email == email)
-        return db.scalars(stmt).first()
+        return await db.scalars(stmt).first()
 
-    def create(self, db: Session, *, obj_in: UserCreate) -> User:
+    async def create(self, db: AsyncSession, *, obj_in: UserCreate) -> User:
         stmt = insert(User).values(
             first_name=obj_in.first_name,
             last_name=obj_in.last_name,
@@ -23,13 +23,13 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             is_active=obj_in.is_active,
             is_superuser=obj_in.is_superuser
         ).returning(User)
-        db_obj = db.scalars(stmt).first()
-        db.commit()
-        db.refresh(db_obj)
+        db_obj = await db.scalars(stmt).first()
+        await db.commit()
+        await db.refresh(db_obj)
         return db_obj
 
-    def update(
-        self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
+    async def update(
+        self, db: AsyncSession, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
     ) -> User:
         if isinstance(obj_in, dict):
             update_data = obj_in
@@ -38,10 +38,10 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         if update_data["password"]:
             hashed_password = get_password_hash(update_data["password"])
             update_data["password"] = hashed_password
-        return super().update(db, db_obj=db_obj, obj_in=update_data)
+        return await super().update(db, db_obj=db_obj, obj_in=update_data)
 
-    def authenticate(self, db: Session, *, email: str, password: str) -> Optional[User]:
-        user = self.get_by_email(db, email=email)
+    async def authenticate(self, db: AsyncSession, *, email: str, password: str) -> Optional[User]:
+        user = await self.get_by_email(db, email=email)
         if not user:
             return None
         if not verify_password(password, user.password):
