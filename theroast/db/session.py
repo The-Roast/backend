af9c04ec730ceb94221d15efd9286befd9736b19
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-08-03 00:28:13 -0700

    Update session.py

--- theroast/db/session.py
+++ theroast/db/session.py
@@ -4,4 +4,4 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 from theroast.config import server_config
 
 engine = create_async_engine(server_config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
-SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, expire_on_commit=False, bind=engine)
