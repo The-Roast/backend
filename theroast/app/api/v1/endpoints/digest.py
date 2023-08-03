9496d8e00d1eb3288296316c72a913c35a51a755
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-08-02 21:53:25 -0700

    Update digest.py

--- theroast/app/api/v1/endpoints/digest.py
+++ theroast/app/api/v1/endpoints/digest.py
@@ -14,24 +14,24 @@ from theroast.db import base, crud
 router = APIRouter()
 
 @router.get("/all", response_model=List[schemas.Digest])
-def read_digests(
+async def read_digests(
     *,
     db: Session = Depends(deps.get_db),
     skip: int = Body(None),
     limit: int = Body(None),
     current_user: base.User = Depends(deps.get_current_active_user)
 ) -> Any:
-    digests = crud.digest.get_multi_by_owner(db, user_uuid=current_user.uuid, skip=skip, limit=limit)
+    digests = await crud.digest.get_multi_by_owner(db, user_uuid=current_user.uuid, skip=skip, limit=limit)
     return digests
 
 @router.get("/{uuid}", response_model=schemas.Digest)
-def read_digest(
+async def read_digest(
     *,
     db: Session = Depends(deps.get_db),
     uuid: UUID,
     current_user: base.User = Depends(deps.get_current_active_user)
 ) -> Any:
-    digest = crud.digest.get(db, uuid=uuid)
+    digest = await crud.digest.get(db, uuid=uuid)
     if not digest:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
@@ -45,24 +45,24 @@ def read_digest(
     return digest
 
 @router.post("/", response_model=schemas.Digest)
-def create_digest(
+async def create_digest(
     *,
     db: Session = Depends(deps.get_db),
     digest_in: schemas.DigestCreate,
     current_user: base.User = Depends(deps.get_current_active_user)
 ) -> Any:
-    digest = crud.digest.create_with_owner(db, obj_in=digest_in, user_uuid=current_user.uuid)
+    digest = await crud.digest.create_with_owner(db, obj_in=digest_in, user_uuid=current_user.uuid)
     return digest
 
 @router.put("/{uuid}", response_model=schemas.Digest)
-def update_digest(
+async def update_digest(
     *,
     db: Session = Depends(deps.get_db),
     uuid: UUID,
     digest_in: schemas.DigestUpdate,
     current_user: base.User = Depends(deps.get_current_active_user)
 ) -> Any:
-    digest = crud.digest.get(db, uuid=uuid)
+    digest = await crud.digest.get(db, uuid=uuid)
     if not digest:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
@@ -73,17 +73,17 @@ def update_digest(
             status_code=HTTPStatus.FORBIDDEN,
             detail="User does not have enough priviledges and does not own digest."
         )
-    digest = crud.digest.update(db, db_obj=digest, obj_in=digest_in)
+    digest = await crud.digest.update(db, db_obj=digest, obj_in=digest_in)
     return digest
 
 @router.delete("/{uuid}", response_model=schemas.Digest)
-def delete_digest(
+async def delete_digest(
     *,
     db: Session = Depends(deps.get_db),
     uuid: UUID,
     current_user: base.User = Depends(deps.get_current_active_user)
 ) -> Any:
-    digest = crud.digest.get(db, uuid=uuid)
+    digest = await crud.digest.get(db, uuid=uuid)
     if not digest:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
@@ -94,5 +94,5 @@ def delete_digest(
             status_code=HTTPStatus.FORBIDDEN,
             detail="User does not have enough priviledges and does not own digest."
         )
-    digest = crud.digest.remove(db, uuid=uuid)
+    digest = await crud.digest.remove(db, uuid=uuid)
     return digest
