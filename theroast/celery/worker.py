30a472b60a136677c2813593ac2377a9a8c433f0
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-08-05 00:08:25 -0700

    Fixed celery

--- theroast/celery/worker.py
+++ theroast/celery/worker.py
@@ -6,4 +6,5 @@ from theroast.config import server_config
 celery_app = Celery(
     __name__, broker=server_config.CELERY_BROKER_URL, backend=server_config.CELERY_BACKEND_URL
 )
+celery_app.conf.update(imports=["theroast.celery.tasks.email"])
 logger = get_task_logger(__name__)
