e625ba4d7e2394a8dd6376189e336a3d5f37b734
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-08-04 20:27:17 -0700

    Updated to make email sending async

--- theroast/core/email.py
+++ theroast/core/email.py
@@ -1,31 +1,8 @@
-from typing import Any, Dict, Optional
 from pathlib import Path
-import emails
-from emails.template import JinjaTemplate
 
 from theroast.config import server_config
+from theroast.celery.tasks.email import send_email
 
-
-def send_email(
-    email_to: str,
-    subject_template: str = "",
-    html_template: str = "",
-    environment: Dict[str, Any] = {},
-) -> None:
-    assert server_config.EMAILS_ENABLED, "no provided configuration for email variables"
-    message = emails.Message(
-        subject=JinjaTemplate(subject_template),
-        html=JinjaTemplate(html_template),
-        mail_from=(server_config.EMAILS_FROM_NAME, server_config.EMAILS_FROM_EMAIL),
-    )
-    smtp_options = {"host": server_config.SMTP_HOST, "port": server_config.SMTP_PORT}
-    if server_config.SMTP_TLS:
-        smtp_options["tls"] = True
-    if server_config.SMTP_USER:
-        smtp_options["user"] = server_config.SMTP_USER
-    if server_config.SMTP_PASSWORD:
-        smtp_options["password"] = server_config.SMTP_PASSWORD
-    response = message.send(to=email_to, render=environment, smtp=smtp_options)
 
 def send_reset_password_email(email_to: str, email: str, token: str) -> None:
     project_name = server_config.PROJECT_NAME
@@ -34,7 +11,7 @@ def send_reset_password_email(email_to: str, email: str, token: str) -> None:
         template_str = f.read()
     server_host = server_config.SERVER_HOST
     link = f"{server_host}/reset-password?token={token}"
-    send_email(
+    send_email.delay(
         email_to=email_to,
         subject_template=subject,
         html_template=template_str,
@@ -53,7 +30,7 @@ def send_new_account_email(email_to: str, first_name: str, username: str, passwo
     with open(Path(server_config.EMAIL_TEMPLATES_DIR) / "create_account.html") as f:
         template_str = f.read()
     link = server_config.SERVER_HOST
-    send_email(
+    send_email.delay(
         email_to=email_to,
         subject_template=subject,
         html_template=template_str,
