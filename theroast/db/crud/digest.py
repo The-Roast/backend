33f9385079beb4c59daa592d9b56f3caf2704516
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-08-15 10:21:38 -0700

    Added sync function versions to crud

--- theroast/db/crud/digest.py
+++ theroast/db/crud/digest.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, insert
+from sqlalchemy.orm import Session
 
 from theroast.db.crud.base import CRUDBase
 from theroast.db.tables.digest import Digest, create_color
@@ -31,6 +32,29 @@ class CRUDDigest(CRUDBase[Digest, DigestCreate, DigestUpdate]):
         db_obj = db_objs.first()
         await db.commit()
         await db.refresh(db_obj)
+        return db_obj
+
+    def sget_multi_by_owner(self, db: Session, *, user_uuid: UUID, skip: Optional[int], limit: Optional[int]) -> List[Digest]:
+        stmt = select(Digest).where(Digest.user_uuid == user_uuid)
+        if skip: stmt = stmt.offset(skip)
+        if limit: stmt = stmt.limit(limit)
+        db_objs = db.scalars(stmt)
+        return db_objs.all()
+
+    def screate_with_owner(self, db: Session, *, obj_in: DigestCreate, user_uuid: UUID) -> Digest:
+        stmt = insert(Digest).values(
+            user_uuid=user_uuid,
+            name=obj_in.name,
+            interests=obj_in.interests,
+            sources=obj_in.sources,
+            personality=obj_in.personality,
+            color=create_color(obj_in.color),
+            is_enabled=obj_in.is_enabled
+        ).returning(Digest)
+        db_objs = db.scalars(stmt)
+        db_obj = db_objs.first()
+        db.commit()
+        db.refresh(db_obj)
         return db_obj
 
     def is_enabled(self, Digest: Digest) -> bool:
--- theroast/db/crud/newsletter.py
+++ theroast/db/crud/newsletter.py
@@ -2,6 +2,7 @@ from typing import Dict, Optional, List
 from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, insert
+from sqlalchemy.orm import Session
 from enum import Enum
 
 from theroast.db.crud.base import CRUDBase
@@ -37,6 +38,35 @@ class CRUDNewsletter(CRUDBase[Newsletter, NewsletterCreate, NewsletterUpdate]):
         db_obj = db_objs.first()
         await db.commit()
         await db.refresh(db_obj)
+        return db_obj
+
+    def sget_multi_by_digest__date(self, db: Session, *, digest_uuid: UUID, skip: Optional[int], limit: Optional[int]) -> List[Newsletter]:
+        stmt = select(Newsletter).where(Newsletter.digest_uuid == digest_uuid).order_by(Newsletter.updated_at.desc())
+        if skip: stmt = stmt.offset(skip)
+        if limit: stmt = stmt.limit(limit)
+        db_objs = db.scalars(stmt)
+        return db_objs.all()
+    
+    def sget_multi_by_digest__clicks(self, db: Session, *, digest_uuid: UUID, skip: Optional[int], limit: Optional[int]) -> List[Newsletter]:
+        stmt = select(Newsletter).where(Newsletter.digest_uuid == digest_uuid).order_by(Newsletter.clicks.desc())
+        if skip: stmt = stmt.offset(skip)
+        if limit: stmt = stmt.limit(limit)
+        db_objs = db.scalars(stmt)
+        return db_objs.all()
+
+    def screate_with_data(self, db: Session, *, obj_in: NewsletterCreate, data: Dict) -> Newsletter:
+        stmt = insert(Newsletter).values(
+            digest_uuid=obj_in.digest_uuid,
+            title=data["title"],
+            introduction=data["introduction"],
+            body=data["body"],
+            conclusion=data["conclusion"],
+            html=data.get("html", None)
+        ).returning(Newsletter)
+        db_objs = db.scalars(stmt)
+        db_obj = db_objs.first()
+        db.commit()
+        db.refresh(db_obj)
         return db_obj
 
     def is_updated(self, Newsletter: Newsletter) -> bool:
