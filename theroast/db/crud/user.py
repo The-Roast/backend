ece6fd0645b0fd3b77adfb9f0354157978b41329
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-08-15 10:13:44 -0700

    Update user.py

--- theroast/db/crud/user.py
+++ theroast/db/crud/user.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, Optional, Union
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, insert
+from sqlalchemy.orm import Session
 
 from theroast.core.security import get_password_hash, verify_password
 from theroast.db.crud.base import CRUDBase
@@ -51,6 +52,49 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         user.is_active = True
         db.add(user)
         await db.commit()
+        return user
+    
+    def sget_by_email(self, db: Session, *, email: str) -> Optional[User]:
+        stmt = select(User).where(User.email == email)
+        scals = db.scalars(stmt)
+        return scals.first()
+
+    def screate(self, db: Session, *, obj_in: UserCreate) -> User:
+        stmt = insert(User).values(
+            first_name=obj_in.first_name,
+            last_name=obj_in.last_name,
+            email=obj_in.email,
+            password=get_password_hash(obj_in.password),
+            is_active=obj_in.is_active,
+            is_superuser=obj_in.is_superuser
+        ).returning(User)
+        scals = db.scalars(stmt)
+        db_obj = scals.first()
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def supdate(
+        self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
+    ) -> User:
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+        if update_data["password"]:
+            hashed_password = get_password_hash(update_data["password"])
+            update_data["password"] = hashed_password
+        return super().supdate(db, db_obj=db_obj, obj_in=update_data)
+
+    def authenticate(self, db: Session, *, email: str, password: str) -> Optional[User]:
+        user = self.get_by_email(db, email=email)
+        if not user:
+            return None
+        if not verify_password(password, user.password):
+            return None
+        user.is_active = True
+        db.add(user)
+        db.commit()
         return user
 
     def is_active(self, user: User) -> bool:
